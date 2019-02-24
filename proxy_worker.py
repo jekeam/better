bdf7ce5a64d84b9902c4bbea0c16857a45184b63
suineg <suinegne@inbox.ru>
2019-02-24 20:17:45 +0500

    Update proxy_worker.py

--- proxy_worker.py
+++ proxy_worker.py
@@ -17,7 +17,7 @@ from utils import DEBUG
 # disable warning
 urllib3.disable_warnings()
 
-TIME_OUT = 3.51
+TIME_OUT = 2
 
 UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3163.100 Safari/537.36'
 
