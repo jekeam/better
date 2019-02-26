46482a18ec6d307ced439d3a71b435c80f2d8f08
suineg <suinegne@inbox.ru>
2019-02-26 23:31:06 +0500

    Update bet_manager.py

--- bet_manager.py
+++ bet_manager.py
@@ -1,24 +1,30 @@
 from exceptions import BetIsLost
 from math import floor
 from utils import prnt
+from time import time
 
 
 class BetManager:
 
-    def __init__(self, obj: dict, vector: str, sc1: int, sc2: int, cur_total: float):
+    def __init__(self, bk: str, obj: dict, vector: str, sc1: int, sc2: int, cur_total: float):
+        if bk in ('fonbet', 'olimp'):
+            self.bk = bk
+        else:
+            raise ValueError('bk not defined, bk=' + bk)
         self.vector = vector
         self.sc1 = int(sc1)
         self.sc2 = int(sc2)
         self.new_sc1 = None
         self.new_sc2 = None
         self.cur_total = cur_total
+        self.time_start = time()
 
         if self.cur_total:
             self.diff_total = max(0, floor(self.cur_total - (self.sc1 + self.sc2)))
 
-        return self.bet(obj)
+        return self.start(obj)
 
-    def bet(self, obj):
+    def start(self, obj):
         if self.diff_total:
             prnt('cur diff_total: ' + str(self.diff_total))
 
@@ -45,7 +51,7 @@ class BetManager:
         if self.sc1 == new_sc1 and self.sc2 == new_sc2 and self.diff_total == 0:
             if self.vector == 'UP':
                 if self.cur_total < new_sc1 + new_sc2:
-                    err_str = " cur_total:{}, new_sc1:{}, new_sc2: {}. Current total lost... I'm sorry..." \
+                    err_str = " cur_total:{}, new_sc1:{}, new_sc2: {}. Current bet lost... I'm sorry..." \
                         .format(str(self.cur_total), str(new_sc1), str(new_sc2))
                     prnt(err_str)
                     BetIsLost(err_str)
@@ -55,7 +61,7 @@ class BetManager:
                     pass
             elif self.vector == 'DOWN':
                 if self.cur_total <= new_sc1 + new_sc2:
-                    err_str = " cur_total:{}, new_sc1:{}, new_sc2: {}. Current total lost... I'm sorry..." \
+                    err_str = " cur_total:{}, new_sc1:{}, new_sc2: {}. Current bet lost... I'm sorry..." \
                         .format(str(self.cur_total), str(new_sc1), str(new_sc2))
                     prnt(err_str)
                     BetIsLost(err_str)
