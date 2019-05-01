5828ad9d5a2fdeca15ed6b6cadda80241c282b47
suineg <suinegne@inbox.ru>
2019-05-01 18:59:08 +0500

    Update server.py

--- server.py
+++ server.py
@@ -32,7 +32,7 @@ def run_server(SERVER_IP, data_json, pair_mathes):
 
                 # attempt create long polling
                 x = 0
-                while self.data == {}:
+                while len(self.data) == 2:
                     print('***********************')
                     print('time.sleep: ' + str(self.data))
                     print(' ')
