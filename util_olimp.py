b4fdbfe3253a5c4de7c7f3632b185fa61f6fefa1
suineg <suinegne@inbox.ru>
2019-05-31 17:17:55 +0500

    195.181.241.195:10600/api/autorize HTTP/1.1

--- util_olimp.py
+++ util_olimp.py
@@ -24,9 +24,9 @@ def get_xtoken_bet(payload):
 
 
 # olimp_url = 'http://08.olimp-proxy.ru:10600'
-olimp_url = 'http://185.112.156.108:10600'
+olimp_url = 'http://195.181.241.195:10600'
 # olimp_url_https = 'https://08.olimp-proxy.ru'
-olimp_url_https = 'https://185.112.156.108:10600'
+olimp_url_https = 'https://195.181.241.195:10600'
 olimp_url_random = 'https://{}.olimp-proxy.ru'  # c 10 по 18й
 
 olimp_secret_key = 'b2c59ba4-7702-4b12-bef5-0908391851d9'
