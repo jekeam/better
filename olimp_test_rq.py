647f445b3e02b34c2dd2cd8ef7007951635c91b1
suineg <suineg@inbox.ru>
2019-08-02 14:59:10 +0300

    test

--- olimp_test_rq.py
+++ olimp_test_rq.py
@@ -1,11 +1,16 @@
 import requests
 
-resp = requests.post(
-    'https://olimpkzapi.ru/api/slice/',
-    headers={'Content-Type': 'application/x-www-form-urlencoded', 'Connection': 'Keep-Alive', 'Accept-Encoding': 'gzip', 'User-Agent': 'okhttp/3.9.1', 'X-TOKEN': '5d237238e0261be09fa4972dab4a6524'},
-    data={'live': 1, 'sport_id': 1, 'platforma': 'ANDROID1', 'lang_id': 0, 'time_shift': 0},
-    proxies={'http': 'https://51.158.119.4:8811', 'https': 'https://51.158.119.4:8811'},
-    verify=False
-)
-
-print(resp.status_code, resp.text)
+with open('olimp.1.proxy', 'r') as f:
+    x = f.readlines()
+    for p in x:
+        url = 'https://olimpkzapi.ru/api/slice/' if 'https' in p else 'http://olimpkzapi.ru/api/slice/'
+        resp = requests.post(
+            url,
+            headers={'Content-Type': 'application/x-www-form-urlencoded', 'Connection': 'Keep-Alive', 'Accept-Encoding': 'gzip', 'User-Agent': 'okhttp/3.9.1', 'X-TOKEN': '5d237238e0261be09fa4972dab4a6524'},
+            data={'live': 1, 'sport_id': 1, 'platforma': 'ANDROID1', 'lang_id': 0, 'time_shift': 0},
+            proxies={'http': p, 'https': p},
+            verify=False,
+            timeout=3
+        )
+        
+        print(p, resp.status_code)
