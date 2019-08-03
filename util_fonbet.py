75a8ccf299ea2309e1d954c71fd7da545b67c4b8
suineg <suinegne@inbox.ru>
2019-08-03 20:01:06 +0500

    change proxy unif

--- util_fonbet.py
+++ util_fonbet.py
@@ -5,11 +5,12 @@ import time
 from exceptions import FonbetMatchСompleted
 from utils import prnts, get_vector, MINUTE_COMPLITE
 import re
+
 url_fonbet = 'https://line-02.ccf4ab51771cacd46d.com'
 url_fonbet_matchs = url_fonbet + '/live/currentLine/en/?2lzf1earo8wjksbh22s'
 url_fonbet_top_matchs = url_fonbet + '/line/topEvents3?place=live&sysId=1&lang=rus&salt=246d6zw3qmkjvjxy8dp'
 # url_fonbet_match = 'https://23.111.80.222/line/eventView?eventId='
-url_fonbet_match = 'https://23.111.18.94/line/eventView?eventId='
+url_fonbet_match = url_fonbet + '/line/eventView?eventId='
 UA = 'Mozilla/5.0 (Windows NT 10; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.3163.100 Safari/537.36'
 fonbet_header = {
     'User-Agent': 'Fonbet/5.2.2b (Android 21; Phone; com.bkfonbet)',
