3f0a2b66b920f6e47ef1cc45db304871c3698c29
suineg <suineg@inbox.ru>
2019-09-05 08:46:52 +0500

    add TT description

--- run.py
+++ run.py
@@ -317,7 +317,7 @@ def starter_bets(bets_olimp, bets_fonbet, pair_mathes, mathes_complite, mathes_i
 
 
 def compare_teams(team1_bk1, team2_bk1, team1_bk2, team2_bk2):
-    print(team1_bk1, team2_bk1, team1_bk2, team2_bk2)
+    # TODO add event group
     if team1_bk1 and team2_bk1 and team1_bk2 and team2_bk2:
         team1_bk1 = re.sub('[^A-z 0-9]', '', team1_bk1.lower()).replace(' ', '')
         team2_bk1 = re.sub('[^A-z 0-9]', '', team2_bk1.lower()).replace(' ', '')
@@ -326,6 +326,7 @@ def compare_teams(team1_bk1, team2_bk1, team1_bk2, team2_bk2):
         if 1.7 < \
                 SequenceMatcher(None, team1_bk1, team1_bk2).ratio() + \
                 SequenceMatcher(None, team2_bk1, team2_bk2).ratio():
+            print(team1_bk1, team2_bk1, team1_bk2, team2_bk2, sep=';')
             return True
     else:
         return False
