24d4bbb5eb6c001431eb4f7b9809ac7e3310f510
Alexander Savinykh <suineg@inbox.ru>
2019-10-22 11:20:28 +0300

    enbale long polling

--- server.py
+++ server.py
@@ -16,15 +16,15 @@ def run_server(SERVER_IP, SERVER_PORT, data_json, pair_mathes, arr_fonbet_top_ma
         def __init__(self, data_json, bar, qux, *args, **kwargs):
             # global json_empt
 
-            prnts('pre data_str: ' + str(len(data_json)) + ', ' + str(data_json))
+            # prnts('pre data_str: ' + str(len(data_json)) + ', ' + str(data_json))
 
             self.end_time = int(time.time()) + 20
             while int(time.time()) < self.end_time and not data_json:
                 if int(time.time()) % 5 == 0:
-                    prnts('wait...' + str(self.end_time-int(time.time())))
+                    # prnts('wait...' + str(self.end_time-int(time.time())))
                     time.sleep(0.5)
             self.data_str = json.dumps(data_json, ensure_ascii=False)
-            prnts('post data_str: ' + str(len(self.data_str)) + ', ' + str(self.data_str))
+            # prnts('post data_str: ' + str(len(self.data_str)) + ', ' + str(self.data_str))
             
             # empty params
             self.bar = bar
