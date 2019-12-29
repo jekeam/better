b97332291d5251d01910b6fecbbda344c54541a8
Ubuntu <suinegne@gmail.com>
2019-12-29 16:37:21 +0000

    bfx runtimeerror-dictionary-changed-size-during-iteration-error

--- util_olimp.py
+++ util_olimp.py
@@ -507,9 +507,9 @@ def get_bets_olimp(bets_olimp, match_id, proxies_olimp, proxy, time_out, pair_ma
 
         try:
             for i in list(bets_olimp):
-                prnts('i: ' + str(prnts(i))
+                prnts('i: ' + str(i))
                 for j in list(bets_olimp[i].get('kofs', {})):
-                    prnts('j: ' + str(prnts(j))
+                    prnts('j: ' + str(j))
                     if round(float(time.time() - float(bets_olimp[i][j].get('time_req', 0)))) > 2.8 and bets_olimp[i][j].get('value', 0) > 0:
                         try:
                             bets_olimp[i]['kofs'][j]['value'] = 0
