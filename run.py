01ac8fd4c93024a82b00cff70fd89c82f41d2510
Ubuntu <suinegne@gmail.com>
2020-01-16 09:43:23 +0000

    bfx

--- run.py
+++ run.py
@@ -445,7 +445,7 @@ def start_seeker_bets_fonbet(bets_fonbet, match_id_fonbet, proxies_fonbet, gen_p
                 raise ValueError('start_seeker_bets_fonbet:' + str(e))
             except Exception as e:
                 exc_type, exc_value, exc_traceback = sys.exc_info()
-                prnts('Exception: Фонбет, ошибка при удалении матча ' + str(match_id_fonbet) + ':' + str(repr(traceback.format_exception(exc_type, exc_value, exc_traceback)))))
+                prnts('Exception: Фонбет, ошибка при удалении матча ' + str(match_id_fonbet) + ':' + str(repr(traceback.format_exception(exc_type, exc_value, exc_traceback))))
         except Exception as e:
             exc_type, exc_value, exc_traceback = sys.exc_info()
             prnts('Exception: Фонбет, ошибка при запросе матча ' + str(match_id_fonbet) + ': ' +
