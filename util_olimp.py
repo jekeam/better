e48dd2f2c67989e8ee817483be26087adf8f9359
jekeam <suinegne@gmail.com>
2020-01-18 02:08:34 +0500

    Update util_olimp.py

--- util_olimp.py
+++ util_olimp.py
@@ -565,8 +565,8 @@ def get_bets_olimp(bets_olimp, match_id, proxies_olimp, proxy, time_out, pair_ma
                         try:
                             bets_olimp[i]['kofs'][j]['value'] = 0
                             bets_olimp[i]['kofs'][j]['factor'] = 0
-                            prnts('2: ' + i + ' ' + j + ' - выставил в 0')
                             if run.DEBUG:
+                                prnts('2: ' + i + ' ' + j + ' - выставил в 0')
                                 prnts('Олимп, матч:' + key_id + ' данные по котировке из БК не получены более ' + str(hide_time) + ' сек., знач. выставил в 0: ' + str(j) + ' ' + str(i))
                         except Exception as e:
                             exc_type, exc_value, exc_traceback = sys.exc_info()
