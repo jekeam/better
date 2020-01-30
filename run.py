ce55f4bb0883bfe76f5db9ea975bb918b92c645e
Ubuntu <ubuntu@ip-172-31-24-151.us-east-2.compute.internal>
2020-01-30 12:23:35 +0000

    large timeout

--- run.py
+++ run.py
@@ -1020,7 +1020,7 @@ def stat_req(stat_req_olimp, stat_req_fonbet):
 
 def mon_cupon(arr_cupon):
     file_name = 'cupon_hist.csv'
-    minute = 15
+    minute = 5
     while True:
         try:
             cur_time = int(round(time.time()))
