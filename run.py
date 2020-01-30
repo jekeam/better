7967a8803908ef3a8fdb1e475ad2d83da9c9bd7f
Ubuntu <ubuntu@ip-172-31-24-151.us-east-2.compute.internal>
2020-01-30 10:29:57 +0000

    ref

--- run.py
+++ run.py
@@ -1035,7 +1035,7 @@ def mon_cupon(arr_cupon):
             df.to_csv(file_name, encoding='utf-8', index=False, sep=';')
             id_old = df[(df['time'] >= (cur_time - (60*minute)))]['cupon_id'].min()
             arr_cupon[0] = str(int((curr_id-id_old)/minute)) + 'куп./мин.'
-            prnts('activity ' + str(arr_cupon[0]) + ' coupons per/sec')
+            prnts('activity ' + str(arr_cupon[0]) + ' coupons per/min')
             time.sleep(time_sleep_proc)
         except Exception as e:
             exc_type, exc_value, exc_traceback = sys.exc_info()
