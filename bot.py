e6098c0d6a73ac0cd67db93255bf28d4a600ff84
Ubuntu <ubuntu@ip-172-31-24-151.us-east-2.compute.internal>
2020-02-07 10:38:42 +0000

    add bot msg

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import subprocess
 def send_msg(msg:str):
     if msg:
         out = subprocess.Popen(
-            ['python3.6', '/home/bot/send_msg.py'], 
+            ['python3.6', '/home/bot/send_msg.py --m "' + msg + '"'], 
             # ['python3.6', '/home/ubuntu/environment/bot/bet_fonbet.py'], 
             stdout=subprocess.PIPE, 
             stderr=subprocess.STDOUT
