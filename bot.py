8a7465870fc4071974f5d09986d60e48be241bb5
root <root@airat.sharipov.fvds.ru>
2020-02-07 16:17:21 +0300

    bfx

--- bot.py
+++ bot.py
@@ -1,14 +1,17 @@
 import subprocess
+import sys
 
 def send_msg(msg:str):
     if msg:
-        ar = '/home/bot/send_msg.py --m "' + msg + '"'
-        print(ar)
         out = subprocess.Popen(
-            ['python3.6', ar], 
+            ['python3.6', '/home/bot/send_msg.py', msg], 
             # ['python3.6', '/home/ubuntu/environment/bot/bet_fonbet.py'], 
             stdout=subprocess.PIPE, 
             stderr=subprocess.STDOUT
         )
-        tdout, stderr = out.communicate()
-        # return int(tdout)
+        stdout, stderr = out.communicate()
+        print(stdout, stderr)
+if __name__=='__main__':
+    arg = sys.argv
+    print(arg)
+    send_msg(arg[1])
