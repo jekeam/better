0166c9f0c6e96aff84f26eb3fb849f36155d8d4a
Ubuntu <ubuntu@ip-172-31-24-151.us-east-2.compute.internal>
2020-02-10 09:26:55 +0000

    bfx

--- run.py
+++ run.py
@@ -856,8 +856,8 @@ def get_forks(forks, forks_meta, pair_mathes, bets_olimp, bets_fonbet, arr_top_m
                                             # TODO_REF
                                             # bet_key_values = v_name + ' ' + str(K) + '/' + str(V)
                                             if bet_key not in arr_values:
-                                                prnts(bet_key + '\npush msg: ' + msg +'\narr: ' + str(value_arr)) # + '\nbet_key_values:' + bet_key_values)
                                                 arr_values.append(bet_key)
+                                                prnts(bet_key + '\npush msg: ' + msg +'\narr: ' + str(value_arr)) # + '\nbet_key_values:' + bet_key_values)
                                                 bot.send_msg(msg)
                                         # skolko stavit (КхV-1)/(К-1)=% от банка.
                         except Exception as e:
