c0e95574d921e29c5215741bee79aff7d841a0d7
Ubuntu <ubuntu@ip-172-31-24-151.us-east-2.compute.internal>
2020-02-10 11:16:53 +0000

    ref

--- run.py
+++ run.py
@@ -851,8 +851,8 @@ def get_forks(forks, forks_meta, pair_mathes, bets_olimp, bets_fonbet, arr_top_m
                                         B = 1/V
                                         C = 1000
                                         val = round((B*(K-1)*C) -((1-B)*C), 2)
-                                        msg = p_vals[2].format(event_type[0:1].upper() + event_type[1:] + ' ' + str(is_top) + '\n'  + 
-                                              'Старт через: ' + str(start_after_min/60) + 'ч.\n' + v_name,
+                                        msg = p_vals[2].format(event_type[0:1].upper() + event_type[1:] + ', risk:' + str(is_top) + '\n'  + 
+                                              'Старт через: ' + str(round(start_after_min/60, 2)) + 'ч.\n' + v_name,
                                               K, 
                                               round(K-V, 2), 
                                               round(val/C*100, 2)
