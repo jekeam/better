b3a11cad418c21b8ae2639cb3608b4c67e8555e6
Ubuntu <ubuntu@ip-172-31-24-151.us-east-2.compute.internal>
2020-03-16 08:37:29 +0000

    add log

--- push_run.py
+++ push_run.py
@@ -1,6 +1,7 @@
 # coding:utf-8
 import subprocess
 import time
+import bot
 
 if __name__=='__main__':
     subprocess.call('systemctl stop scan.service', shell=True)
@@ -10,3 +11,4 @@ if __name__=='__main__':
     subprocess.call('python3.6 proxy_push.py', shell=True, cwd='/home/scan/')
     time.sleep(5)
     subprocess.call('systemctl restart scan.service', shell=True)
+    bot.send_msg('Сканер выполнил суточное обновление')
