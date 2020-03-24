8622e06f7d69ec2d64bed8f73afd9b67b0840919
Alexander Savinykh <suinegne@gmail.com>
2020-03-25 01:50:40 +0500

    Update run.py

--- run.py
+++ run.py
@@ -1222,6 +1222,16 @@ def get_forks(forks, forks_meta, pair_mathes, bets, arr_top_matchs, arr_values):
                                         'fonbet_maxbet_fact': {},
                                         'place': type_time,
                                     }
+
+                                    if L <= 0.97 and type_time == 'pre':  # start_after_min <= 60 * 20
+                                        msg = event_type[0:1].upper() + event_type[1:] + ', лига:' + str(is_top) + '\n' + v_name + '\n'
+                                        if msg not in arr_values:
+                                            msg = msg + \
+                                                  str(round(1 - L, 2) * 100) + '%, ' + \
+                                                  name_bk1 + '=' + str(k_bk1) + ', ' + \
+                                                  name_bk2 + '=' + str(k_bk2) + '\n' + \
+                                                  'Старт через: ' + str(round(start_after_min / 60, 1)) + 'ч.'
+                                            bot.send_msg(msg)
                             else:
                                 try:
                                     # print('del: ' + str(bet_key) + ' fork_hide_time: ' + str(fork_hide_time) + ' deff_time:' + str(deff_time))
