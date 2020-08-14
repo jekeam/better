ddb5d41e8b35d596e2e4b0cb2afad5645f7d2ad1
Alexander S <suinegne@gmail.com>
2020-08-14 18:59:33 +0500

    Update restart_run.py

--- restart_run.py
+++ restart_run.py
@@ -8,3 +8,4 @@ if __name__=='__main__':
     subprocess.call('python3.6 proxy_push.py', shell=True, cwd='/home/scan/')
     time.sleep(5)
     subprocess.call('systemctl restart scan.service', shell=True)
+    subprocess.call('systemctl restart bot.service', shell=True)
